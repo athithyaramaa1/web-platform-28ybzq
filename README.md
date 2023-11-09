0374bed44ececcf9bc930d0e5b2702f39b25f418
athithyaramaa1 <149147655+athithyaramaa1@users.noreply.github.com>
2023-11-09 09:34:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-28ybzq
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/athithyaramaa1/web-platform-28ybzq)
---
+++ script.js
@@ -0,0 +1,33 @@
+// Challenge 1: Store the string "I am a" in one variable and store the other string "Kalvian" in other variable and concatenate both the string to get the output as "I am a Kalvain" using "+" operator.
+
+var a = "I am a";
+var b = "Kalvian";
+console.log(a + " "+ b)
+// Challenge 2: Store the given string in a variable and print the same in console. String: If you fail, never give up because FAIL means "First Attempt In Learning".
+var motivation =  "If you fail, never give up because FAIL means" + "First Attempt In Learning";
+console.log(motivation) 
+// Challenge 3: Store a string in a variable and display the length of the string.
+var myBeautifulName = "Athithya";
+console.log(myBeautifulName.length)
+// Challenge 4: Store a string in a variable and convert it into uppercase.
+var x = "I AM A FULL STACK DEVELOPER";
+console.log(x.toLowerCase())
+//Challenge 5: Store the string "I love programming" in a variable and replace the word programming with "JavaScript programming".
+var kal = "I love programming";
+console.log(kal.replace(/programming/g, "JavaScript programming"))
+// Challenge 6: Store a string in variable and print the string in console 3 times using repeat method.
+var yay = "I love learning in Kalvium!";
+console.log(yay.repeat(3))
+//Challenge 7: Store the string as "I am a Kalvian" in a variable and convert the string into array and print the array in console.
+var kalvian = "I am a Kalvian";
+console.log(kalvian.split())
+// Challenge 8: Store the given string "Time and Tide wait for none" in a variable and using indexOf method find the index of "Ti" and print the same in console.
+
+// Challenge 9: Store the given string "When life gives you lemons make lemonade" in a variable and find whether it includes "lemon".
+var lemonLife = "When life gives you lemons make lemonade";
+console.log(lemonLife.includes("lemon"))
+// Challenge 10: Store the given string "You must be the change you wish to see in the world." in a variable and use slice to console log the word "change" and use substring method to console log the word "world".
+
+var whoohoo = "You must be the change you wish to see in the world.";
+console.log(whoohoo.slice(16, 22))
+console.log(whoohoo.substring(45,51))
